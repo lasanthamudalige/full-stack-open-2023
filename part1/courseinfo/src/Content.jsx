a491f6cf26d94fb1403196c081f6afa2de777c12
Lasantha <lasantha1130@gmail.com>
2023-10-03 20:54:47 +0530

    code improvement

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -5,8 +5,8 @@ const Content = (props) => {
         <>
             {course.parts.map(part => {
                 return (
-                    // Make 'p' tags for  each course(course name and no of exercises) and pass array index as the key of the
-                    <p key={props.course.parts.indexOf(part)}>{part.name} {part.exercises}</p> 
+                    // Make 'p' tags for  each course(course name and no of exercises) and pass array index as the key of the part
+                    <p key={course.parts.indexOf(part)}>{part.name} {part.exercises}</p> 
                 )
             })}
         </>
