5712bfb1704746b14068a238ba130da52a7ad883
Lasantha <lasantha1130@gmail.com>
2023-10-05 14:39:27 +0530

    complete anecdotes exercise

---
+++ part1/anecdots/src/Votes.jsx
@@ -0,0 +1,13 @@
+const Votes = (props) => {
+    const points = props.points
+    const itemIndex = props.selected
+
+    return (
+        <>
+            {/* If vote == 1 render "vote" else render "votes" */}
+            <p>Has {points[itemIndex]} {points[itemIndex] === 1 ? <>vote</> : <>votes</>}</p>
+        </>
+    )
+}
+
+export default Votes
