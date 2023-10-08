c9eaffbef1a6b034ef0ee0b0e29fa347eca0d930
Lasantha <lasantha1130@gmail.com>
2023-10-08 22:52:14 +0530

    add div to replace fragments inside return

--- part2/courseinfo2/src/App.jsx
+++ part2/courseinfo2/src/App.jsx
@@ -47,12 +47,12 @@ const App = () => {
   ]
 
   return (
-    <>
+    <div>
       <h1>Web development curriculum</h1>
       {courses.map(course =>
         <Course key={course.id} course={course} />
       )}
-    </>
+    </div>
   )
 }
 
